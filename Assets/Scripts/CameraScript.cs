784ec07b4b1506e695c44c650e66596375c082ba
GatekeeperCode <115508099+GatekeeperCode@users.noreply.github.com>
2024-02-05 20:27:52 -0500

    CameraChaos

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -5,17 +5,36 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     OrpheusScript Orpheus;
+    EurydiceScript Eurydice;
 
     Vector3 offset = new Vector3(0, 0, -10);
     // Start is called before the first frame update
     void Start()
     {
         Orpheus = FindObjectOfType<OrpheusScript>();
+        Eurydice = FindObjectOfType<EurydiceScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(Orpheus.transform.position.x + offset.x, Orpheus.transform.position.y + offset.y, offset.z);
+        transform.position = new Vector3((Orpheus.transform.position.x + Eurydice.transform.position.x)/2 + offset.x, (Orpheus.transform.position.y+ Eurydice.transform.position.y) /2 + offset.y, offset.z);
+
+        if (Orpheus.transform.position.x - Eurydice.transform.position.x > 25 || Eurydice.transform.position.x - Orpheus.transform.position.x > 25)
+        {
+            gameObject.GetComponent<Camera>().orthographicSize = 8;
+        }
+        else if (Orpheus.transform.position.x - Eurydice.transform.position.x > 20 || Eurydice.transform.position.x - Orpheus.transform.position.x > 20)
+        {
+            gameObject.GetComponent<Camera>().orthographicSize = 7;
+        }
+        else if (Orpheus.transform.position.x - Eurydice.transform.position.x > 15 || Eurydice.transform.position.x - Orpheus.transform.position.x > 15)
+        {
+            gameObject.GetComponent<Camera>().orthographicSize= 6;
+        }
+        else
+        {
+            gameObject.GetComponent<Camera>().orthographicSize = 5;
+        }
     }
 }
