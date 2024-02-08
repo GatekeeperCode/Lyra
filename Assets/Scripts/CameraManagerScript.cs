76e5b15e260650bb7420155e5b74fef7bf65a90d
GatekeeperCode <115508099+GatekeeperCode@users.noreply.github.com>
2024-02-07 20:55:43 -0500

    SplitScreenSetUp

--- Assets/Scripts/CameraManagerScript.cs
+++ Assets/Scripts/CameraManagerScript.cs
@@ -6,17 +6,32 @@ public class CameraManagerScript : MonoBehaviour
 {
     public GameObject _singlePlayerCam1;
     public GameObject _singlePlayerCam2;
+    bool isSinglePlayer;
+    public GameObject _splitCam1;
+    public GameObject _splitCam2;
 
     // Start is called before the first frame update
     void Start()
     {
-        _singlePlayerCam2.SetActive(false);
+        if(PlayerPrefs.GetInt("playerCount")==2)
+        {
+            _singlePlayerCam2.SetActive(false);
+            _singlePlayerCam1.SetActive(false);
+            isSinglePlayer = false;
+            _splitCam1.SetActive(true);
+            _splitCam1.SetActive(true);
+        }
+        else
+        {
+            _singlePlayerCam2.SetActive(false);
+            isSinglePlayer = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && isSinglePlayer)
         {
             _singlePlayerCam2.SetActive(!_singlePlayerCam2.activeSelf);
             _singlePlayerCam1.SetActive(!_singlePlayerCam1.activeSelf);
