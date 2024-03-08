258d5caf0e0e7631afb6a3123216c23d35ea0f89
Jules-1948 <schamingjc20@gcc.edu>
2024-03-08 01:13:13 -0500

    Updated Character physics and camera movement

--- Assets/Scripts/CameraManagerScript.cs
+++ Assets/Scripts/CameraManagerScript.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class CameraManagerScript : MonoBehaviour
 {
+    public GameObject Orpheus;
+    public GameObject Eurydice;
     public GameObject _singlePlayerCam1;
     public GameObject _singlePlayerCam2;
-    bool isSinglePlayer;
     public GameObject _splitCam1;
     public GameObject _splitCam2;
+    public bool orphView = true;
+    bool isSinglePlayer;
+    Vector3 camOffset = new Vector3(0, 0, -10);
 
     // Start is called before the first frame update
     void Start()
@@ -34,10 +38,31 @@ public class CameraManagerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Check for camera switching
         if(Input.GetKeyDown(KeyCode.Space) && isSinglePlayer)
         {
+            orphView = !orphView;
             _singlePlayerCam2.SetActive(!_singlePlayerCam2.activeSelf);
             _singlePlayerCam1.SetActive(!_singlePlayerCam1.activeSelf);
         }
+
+        // Center cameras on players
+        if(isSinglePlayer)
+        {
+            if (orphView)
+            {
+                characterCenter(Orpheus, _singlePlayerCam1);
+            }
+            else
+            {
+                characterCenter(Eurydice, _singlePlayerCam2);
+            }
+        }
+        
+    }
+
+    private void characterCenter(GameObject character, GameObject camera)
+    {
+        camera.transform.position = new Vector3(character.transform.position.x + camOffset.x, character.transform.position.y + camOffset.y, camOffset.z);
     }
 }
