b1803acf8cdceb208b71929c4efc97ec768853de
Jules-1948 <schamingjc20@gcc.edu>
2024-03-19 14:03:26 -0400

    Some more Orpheus walls

--- Assets/Scripts/FloorScript.cs
+++ Assets/Scripts/FloorScript.cs
@@ -8,23 +8,6 @@ public class FloorScript : MonoBehaviour
     public float[] wallTop;
     public float[] wallBottom;
     public bool[] setFalse;
-    bool onFloor = false;
-
-    public GameObject wall1;
-    public GameObject wall2;
-    public GameObject wall3;
-    public GameObject wall4;
-    public GameObject wall5;
-    public float w1Top;
-    public float w1Bottom;
-    public float w2Top;
-    public float w2Bottom;
-    public float w3Top;
-    public float w3Bottom;
-    public float w4Top;
-    public float w4Bottom;
-    public float w5Top;
-    public float w5Bottom;
 
 
     OrpheusScript orpheus;
@@ -47,10 +30,6 @@ public class FloorScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!orpheus.IsOnFloor())
-        {
-            return;
-        }
 
         for(int i = 0; i < walls.Length; i++)
         {
@@ -63,11 +42,8 @@ public class FloorScript : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (!orpheus.IsOnFloor())
-        {
-            return;
-        }
-        else if (orpheus._lyreRaise)
+
+        if (orpheus._lyreRaise)
         {
             for (int i = 0; i < walls.Length; i++)
             {
