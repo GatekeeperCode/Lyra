3498f75a939245d1c84affdb74bfad796b775a56
GatekeeperCode <115508099+GatekeeperCode@users.noreply.github.com>
2024-04-30 13:07:18 -0400

    Fixing an issue

---
+++ Assets/Scripts/CamDissapearVariable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamDissapearVariable : MonoBehaviour
+{
+    //This is dumb, I know, but it's a way I thought of to fix an issue i have been struggling with for too long
+    public bool _camOff;
+}
